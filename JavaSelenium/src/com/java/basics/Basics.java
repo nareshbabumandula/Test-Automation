78116ee8caf8844aec14480b4f33fe354feede59
nareshbabumandula <naresh223@gmail.com>
2023-02-21 09:21:08 +0530

    Added JavaSeleneium project

---
+++ JavaSelenium/src/com/java/basics/Basics.java
@@ -0,0 +1,18 @@
+package com.java.basics;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world..!");
+		// eclipse shortcuts
+		/*
+		 * sysout ctrl+spacebar 
+		 * For comment use //
+		 * For commenting block of code --> ctrl+shift+/
+		 * Code Indentation - Ctrl+A then Ctrl+I 
+		 * Run Java Application - Click on Run button or Right click on code -> Run As -> Java Application or Ctrl+F11
+		 */	
+	
+	}
+
+}
---
+++ JavaSelenium/src/com/java/basics/DataTypes.java
@@ -0,0 +1,17 @@
+package com.java.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Primitive data types - 8 types (byte, short, int, long, float, double, char, boolean)
+		byte b = 127;
+		System.out.println("Byte value is : " + b);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		short s = 23455;
+		System.out.println("Short value is : " + s);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+	}
+
+}
