a530fbb889c451432de20dc7a358ec9bfd87a5f9
padmamqa <rkpadmasri@gmail.com>
2023-02-21 19:33:35 -0800

    MinMax value class added.

---
+++ JavaSelenium/src/com/java/basics/padma/MinMaxValue.java
@@ -0,0 +1,33 @@
+package com.java.basics.padma;
+
+public class MinMaxValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b=127;
+		System.out.println("Byte value of b:"+b);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		short s=25127;
+		System.out.println("Short value of s:"+s);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		int i=251270;
+		System.out.println("Integer value of i:"+i);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		long l=2512788;
+		System.out.println("Long value of l:"+l);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		float f=(float) 102.88;
+		System.out.println("float value of f:"+f);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		double d=488.898;
+		System.out.println("Double value of d:"+d);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+
+	}
+}
