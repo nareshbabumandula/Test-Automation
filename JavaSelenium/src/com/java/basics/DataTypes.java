d28447b807b22f349fb578d6f074ba111713a1fa
nareshbabumandula <naresh223@gmail.com>
2023-02-22 08:22:03 +0530

    22-Feb-2023: Naresh: Added examples for char and boolean datatypes

--- JavaSelenium/src/com/java/basics/DataTypes.java
+++ JavaSelenium/src/com/java/basics/DataTypes.java
@@ -8,7 +8,7 @@ public class DataTypes {
 		System.out.println("Byte value is : " + b);
 		System.out.println(Byte.MIN_VALUE);
 		System.out.println(Byte.MAX_VALUE);
-		short s = 23455;
+		short s = 32767;
 		System.out.println("Short value is : " + s);
 		System.out.println(Short.MIN_VALUE);
 		System.out.println(Short.MAX_VALUE);
@@ -16,7 +16,21 @@ public class DataTypes {
 		System.out.println("Integer value is : " + s);
 		System.out.println(Integer.MIN_VALUE);
 		System.out.println(Integer.MAX_VALUE);
-		
+		long l=2512788;
+		System.out.println("Long value of l:"+l);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		float f=(float) 102.88;
+		System.out.println("float value of f:"+f);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		double d=488.898;
+		System.out.println("Double value of d:"+d);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		char c = 'h';
+		System.out.println("Character value of c is : " + c);
+		boolean bFlag = true;
+		System.out.println("Boolean value of bFlag is : " + bFlag);
 	}
-
 }
