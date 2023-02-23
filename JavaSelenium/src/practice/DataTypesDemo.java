0113d83b3078d4fbe7636bbd9603e48c1c950d84
ratna <ratna@Bunny>
2023-02-23 07:44:59 +0530

    23-Feb-2023: Keerthana: Added non primitive datatype example

---
+++ JavaSelenium/src/practice/DataTypesDemo.java
@@ -0,0 +1,37 @@
+package practice;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// Primitive data types - 8 types (byte, short, int, long, float, double, char, boolean)
+		byte b = 85;
+		System.out.println("Byte value is : " + b);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		short s = 32566;
+		System.out.println("Short value is : " + s);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		int i = 34444;
+		System.out.println("Integer value is : " + i);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		long l=245888;
+		System.out.println("Long value of l:"+l);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		float f=(float) 110.98;
+		System.out.println("float value of f:"+f);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		double d=478.668;
+		System.out.println("Double value of d:"+d);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
+		// Non primitive datatypes
+		String str = "hello world";
+		System.out.println(str);
+		
+	}
+}
