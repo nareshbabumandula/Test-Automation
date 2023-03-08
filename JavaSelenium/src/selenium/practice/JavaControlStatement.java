8e3c087b357c6d9e13d342a5e6324dba3f845fa6
USER <USER@PRUDHVI-NADH.mshome.net>
2023-03-08 09:26:19 +0530

    08-Mar-2023: Prudhvi: Practiced with Switch Statement by creating Month.

--- JavaSelenium/src/selenium/practice/JavaControlStatement.java
+++ JavaSelenium/src/selenium/practice/JavaControlStatement.java
@@ -43,12 +43,59 @@ public class JavaControlStatement {
 			break;
 		}
 	}
+	
+	void switchTestMonth(String mnth) {
+		switch (mnth.trim()) {
+		case "jan" :
+			System.out.println("January");
+			break;
+		case "feb" :
+			System.out.println("February");
+			break;
+		case "mar" :
+			System.out.println("    March");
+			break;
+		case "apr" :
+			System.out.println("April");
+			break;
+		case "may" :
+			System.out.println("May");
+			break;
+		case "jun" :
+			System.out.println("June");
+			break;
+		case "jul" :
+			System.out.println("July");
+			break;
+		case "aug" :
+			System.out.println("August");
+			break;
+		case "sep" :
+			System.out.println("September");
+			break;
+		case "oct" :
+			System.out.println("October    ");
+			break;
+		case "nov" :
+			System.out.println("November");
+			break;
+		case "dec" :
+			System.out.println("Dcember");
+			break;
+		default :
+			System.out.println("Enter a Valid Month.");
+			break;
+		}
+		
+	}
+
 
 	public static void main(String[] args) {
 		JavaControlStatement cs =new JavaControlStatement();
 		cs.ifTest();
+		cs.switchTestMonth("mar");
 		cs.switchTest("sat");
-		cs.switchTest("SaTu");
+		
 
 	}
 
