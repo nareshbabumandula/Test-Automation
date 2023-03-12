307786111ed34d7ed440c27695d75af23c7b9313
my pc <my pc@DESKTOP-CP496NV>
2023-03-12 08:35:28 +0530

    java selenium commit

--- JavaSelenium/src/practice/Demo.java
+++ JavaSelenium/src/practice/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Demo class..!");
 		System.out.println("Test");
-
+		System.out.println("Java Selenium");
 	}
 
 }
