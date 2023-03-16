bec6db8e07590f99e0e7c525e292f8613d5e9cbf
NirmalaBalakrishna07 <nirmalabalakrishna2022@gmail.com>
2023-03-16 23:30:39 +0530

    commit methodoverride

---
+++ JavaSelenium/src/com/oops/MethodOverrideTest.java
@@ -0,0 +1,14 @@
+package com.oops;
+
+public class MethodOverrideTest {
+	
+	int a=15;
+	int b=25;
+	
+	void addition() {
+		int c=a+b;
+		System.out.println("Addition of a and b in super class is : " +c);
+	}
+	
+
+}
