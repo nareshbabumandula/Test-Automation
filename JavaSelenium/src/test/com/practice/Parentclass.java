d62406fce00e8a5beeab2d4f9b10f42a15db5ca2
ratna <ratna@Bunny>
2023-03-20 07:32:43 +0530

    upcasting and downcasting

---
+++ JavaSelenium/src/test/com/practice/Parentclass.java
@@ -0,0 +1,34 @@
+package test.com.practice;
+
+public class Parentclass{
+
+	String name;
+	void display() {
+		System.out.println();
+	}
+	String colour;
+	void colour() {
+		System.out.println();
+			}
+	byte a=12;
+	byte b=14;
+	byte c=46;
+	byte d=78;
+	byte e=80;
+	byte f=22;
+
+	void addition() {
+		int Z=a+b;
+		System.out.println("Addition of a and b="+Z);
+		int z=c+d;
+		System.out.println("Addition of c and d="+Z);
+	}
+	void division() {
+		int Z = e/a;
+		System.out.println("Division of e/a = " +Z );
+	}
+	void multiplication() {
+		int z = a * b;
+		System.out.println("multiplication of a*b="+z);
+	}
+}
