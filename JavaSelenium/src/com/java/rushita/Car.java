37b8098287686fbe1066392b3472deedead6f78c
my pc <my pc@DESKTOP-CP496NV>
2023-03-20 07:32:02 +0530

    upcasting and downcasting ass

---
+++ JavaSelenium/src/com/java/rushita/Car.java
@@ -0,0 +1,21 @@
+package com.java.rushita;
+
+public class Car extends Vehicle{
+	static int maxSpeed=300;
+
+	public static void main(String[] args) {
+		
+		System.out.println("maxSpeed:" +maxSpeed);
+		
+		Vehicle V1=new Car();
+		System.out.println(V1.maxSpeed);
+		
+		Car c=new Car();
+		Vehicle V2= c;
+		Car car= (Car)V2;;
+		System.out.println(car.maxSpeed);
+	
+	}
+
+}
+
---
+++ JavaSelenium/src/com/java/rushita/Vehicle.java
@@ -0,0 +1,14 @@
+package com.java.rushita;
+
+public class Vehicle {
+
+	static int maxSpeed=250;
+
+	public static void main(String[] args) {
+
+		System.out.println("maxSpeed:" +maxSpeed);
+
+	}
+}
+
+
