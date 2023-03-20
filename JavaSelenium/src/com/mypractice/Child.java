b30385633499d145fa1db3b09b91233a75a27858
admin <admin@DESKTOP-RC2OT1J>
2023-03-20 08:11:03 +0530

    In upcasting Parent class reference object cannot call Child class variables

--- JavaSelenium/src/com/mypractice/Child.java
+++ JavaSelenium/src/com/mypractice/Child.java
@@ -4,6 +4,7 @@ public class Child extends Parent {
 	
 	String name = "Child";
     int age = 5;
+    String gender = "Baby Boy";
 	void print() {
 		System.out.println("Child class method is called");
 	}
@@ -16,6 +17,7 @@ public class Child extends Parent {
 		System.out.println(p.name);//calling Parent class variable
 		System.out.println(p.age);//calling Parent class variable
 		System.out.println(p.noOfChilds);//calling Parent class variable
+		//System.out.println(p.gender);
 		p.print();//calling Parent class method
 		//Overriding
 		System.out.println("***Overriding***");
@@ -23,6 +25,7 @@ public class Child extends Parent {
 		System.out.println(c.name);
 		System.out.println(c.age);
 		System.out.println(c.noOfChilds);
+		System.out.println(c.gender);
 		c.print();
 		//Upcasting
 		System.out.println("***Upcasting***");
@@ -30,6 +33,7 @@ public class Child extends Parent {
 		System.out.println(p1.name);//calling Parent class variable
 		System.out.println(p1.age);//calling Parent class variable
 		System.out.println(p1.noOfChilds);//calling Parent class variable
+		//System.out.println(p1.gender);In upcasting Parent class reference object cannot call Child class variables 
 		p1.print();//calling Child class method
 		//Downcasting
 		System.out.println("***Downcasting***");
@@ -38,6 +42,7 @@ public class Child extends Parent {
 		System.out.println(c1.name);//calling Child class variable 
 		System.out.println(c1.age);//calling Child class variable
 		System.out.println(c1.noOfChilds);//calling Parent class variable because this variable didn't declare in Child class
+		System.out.println(c1.gender);
 		c1.print();//calling Child class method
 		
 	}
