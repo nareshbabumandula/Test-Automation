ddfa03e23d3c42577fb7fa940d1b45f3660856f5
nareshbabumandula <naresh223@gmail.com>
2023-04-13 08:46:45 +0530

    13-Apr-2023: Naresh: Working with dropdown list in Selenium

--- JavaSelenium/src/com/selenium/scripts/DropdownListboxTest.java
+++ JavaSelenium/src/com/selenium/scripts/DropdownListboxTest.java
@@ -1,11 +1,14 @@
 package com.selenium.scripts;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,7 +16,7 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-public class CheckboxTest {
+public class DropdownListboxTest {
 
 	static ExtentTest test;
 	static ExtentReports report;
@@ -31,37 +34,52 @@ public class CheckboxTest {
 		System.out.println("Launched the browser..!");
 		// Extent Reports
 		report = new ExtentReports("./ExtentReportResults.html");
-		test = report.startTest("Checkbox Methods");
+		test = report.startTest("Dropdown listbox Methods");
 	}
 
 	@Test
-	public void checkboxMethods() throws InterruptedException {
+	public void dropdownListboxMethods() throws InterruptedException {
 		boolean bFlag=false;
 		try {
 			driver.manage().window().maximize();
 			driver.findElement(By.linkText("Sample Forms")).click();
-			driver.findElement(By.xpath("//input[@value='Second Check Box']")).isDisplayed();
-			WebElement checkbox = driver.findElement(By.xpath("//input[@value='Second Check Box']"));
-			System.out.println(checkbox.getAttribute("type"));
-			System.out.println(checkbox.getAttribute("name"));
-			System.out.println(checkbox.getAttribute("value"));
-			System.out.println(checkbox.getTagName());
-			System.out.println("Checkbox enable status before clicking is : " + checkbox.isSelected());
-			checkbox.click();
-			System.out.println("Display status of the checkbox is : " + checkbox.isDisplayed());
-			System.out.println("Checkbox enable status after clicking is : " + checkbox.isSelected());
+			WebElement predefinedCountries = driver.findElement(By.id("q9"));
 			
-			if(checkbox.isSelected()){
-				test.log(LogStatus.PASS, "Successfully clicked on checkbox");
-				System.out.println("Successfully clicked on checkbox");
+			Select select = new Select(predefinedCountries);
+			String selectedCountryBefore = select.getFirstSelectedOption().getText();
+			System.out.println("By default the couontry selected is : " +selectedCountryBefore);
+			select.selectByIndex(0);
+			Thread.sleep(2000);
+			select.selectByIndex(1);
+			Thread.sleep(2000);
+			
+			List<WebElement> dropdownitems = select.getOptions();
+			System.out.println("No of countries displayed in the country dropdown are : " + dropdownitems.size());
+			for (int i = 0; i < dropdownitems.size(); i++) {
+				String value = dropdownitems.get(i).getText();
+				System.out.println(value);
+				if (value.equals("India")) {
+					break;
+				}
+			}
+			System.out.println("Looping through lambda expression");
+			dropdownitems.forEach(country-> System.out.println(country.getText()));
+			
+			select.selectByVisibleText("India");
+			System.out.println(select.isMultiple());
+			
+			String selectedCountry = select.getFirstSelectedOption().getText();
+
+			if(selectedCountry.equals("India")){
+				test.log(LogStatus.PASS, "Successfully selected value from a dropdown");
+				System.out.println("Successfully selected value from a dropdown");
 				bFlag=true;	
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
 			String message = e.getMessage();
 			String errorMsg[] = message.split("}");
-			test.log(LogStatus.FAIL, "Failed to click on checkbox since - " + errorMsg[0]);
-			System.out.println("Failed to click on checkbox since - " + errorMsg[0]);
+			test.log(LogStatus.FAIL, "Failed to select value from a dropdown since - " + errorMsg[0]);
+			System.out.println("Failed to select value from a dropdown since - " + errorMsg[0]);
 		}finally {
 			if (bFlag) {
 				test.log(LogStatus.PASS, "Test Case Executed Successfully..!");
