2385d9a10ddccee927691c7c21f6f6f8c0db4313
USER <USER@PRUDHVI-NADH>
2023-04-24 00:00:06 +0530

    23-Apr-2023: Prudhvi:

---
+++ JavaSelenium/src/selenium/practice/testng/BaseTestAnnotations.java
@@ -0,0 +1,55 @@
+package selenium.practice.testng;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import org.testng.annotations.BeforeClass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+
+public class BaseTestAnnotations {
+
+	ExtentReports report;
+	ExtentTest test;
+	
+	WebDriver driver;
+
+	@BeforeSuite
+	public void creatingExtentReport() {
+		//Extent Report
+		report = new ExtentReports("./ExtentReportResults.html");
+		test = report.startTest("TC001");
+		System.out.println("BeforeSuite : Created the Extent Report.");
+	}
+
+	@BeforeTest
+	public void launchBrowser() {
+		System.setProperty("WebDriver.edgedriver.driver","./drivers/msedgedriver.exe");
+		EdgeOptions option = new EdgeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		driver = new EdgeDriver(option);
+		driver.manage().window().maximize();
+		//driver.get("https://www.google.com");
+		System.out.println("BeforeTest : Launched the Edge Browser.");
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+		System.out.println("AfterTest : CLosed the Edge Browser.");
+	}
+
+	@AfterSuite
+	public void teardown() {
+		report.endTest(test);
+		report.flush();
+		System.out.println("AfterSuite : Extent Report has generated.");
+	}
+
+}
--- JavaSelenium/src/selenium/practice/testng/TestAnnotaions.java
+++ JavaSelenium/src/selenium/practice/testng/TestAnnotaions.java
@@ -1,58 +1,98 @@
 package selenium.practice.testng;
 
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 
-public class TestAnnotaions {
-  @Test
-  public void testMethod() {
-	  System.out.println("Test Method passed.");
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-	  System.out.println("beforeMethod.");
-  }
+public class TestAnnotaions extends BaseTestAnnotations {
 
-  @AfterMethod
-  public void afterMethod() {
-	  System.out.println("afterMethod.");
-  }
+	@BeforeMethod
+	public void beforeMethod() throws Exception {
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone14 pro");
+		driver.findElement(By.cssSelector("input#nav-search-submit-button")).click();
+		String e =driver.findElement(By.xpath("//span[contains(text(),'iphone14 pro')]")).getText();
+		if(e.contains("iphone14")) {
+			test.log(LogStatus.PASS,"Successfully searched Iphone 14 pro.");  
+		}else {
+			test.log(LogStatus.FAIL,"Failed to search Iphone 14 pro.");
+		}
+		System.out.println("beforeMethod.");
+	}
 
-  @BeforeClass
-  public void beforeClass() {
-	  System.out.println("beforeClass.");
-  }
+	@Test
+	public void testMethod() throws IOException, Exception {
+		String parentTab = driver.getWindowHandle();
+		Thread.sleep(3000);
+		WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Apple iPhone 14 Pro (128 GB) - Gold')]"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();",element);
+		Set<String> WindowHandles = driver.getWindowHandles();
+		for(String tab : WindowHandles) {
+			if(!tab.equals(parentTab)) {
+				driver.switchTo().window(tab);
+				Actions action = new Actions(driver);
+				WebElement purple = driver.findElement(By.xpath("//input[@name='0']"));
+				action.moveToElement(purple).perform();
+				Thread.sleep(1000);
+				WebElement silver = driver.findElement(By.xpath("//input[@name='2']"));
+				action.moveToElement(silver).perform();
+				Thread.sleep(1000);
+				WebElement black = driver.findElement(By.xpath("//input[@name='3']"));
+				action.moveToElement(black).perform();
+				Thread.sleep(1000);
+				js.executeScript("arguments[0].click();",purple);
+				WebElement storagetb = driver.findElement(By.xpath("//input[@name='0'][@aria-labelledby='size_name_0-announce']"));
+				action.moveToElement(storagetb).perform();
+				storagetb.click();
+				String finalVariant = driver.findElement(By.id("productTitle")).getText();
+				if(finalVariant.contains("1 TB")) {
+					test.log(LogStatus.PASS,"Successfully finalised the variant of Iphone 14 pro.");  
+				}else {
+					test.log(LogStatus.FAIL,"Failed to finalise the variant of Iphone 14 pro.");
+				}
+			}
+		}
+		Desktop.getDesktop().browse(new File("ExtentReportResults.html").toURI());
+		System.out.println("Test Method passed.");
+	}
 
-  @AfterClass
-  public void afterClass() {
-	  System.out.println("afterClass.");
-  }
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("afterMethod.");
+		//test.log(LogStatus.PASS,"Successfully executed AfterMethod.");
+	}
 
-  @BeforeTest
-  public void beforeTest() {
-	  System.out.println("beforeTest.");
-  }
+	@BeforeClass
+	public void accessSite() {
+		driver.get("https://www.Amazon.in");
+		System.out.println("beforeTest.");
+	}
 
-  @AfterTest
-  public void afterTest() {
-	  System.out.println("afterTest.");
-  }
-
-  @BeforeSuite
-  public void beforeSuite() {
-	  System.out.println("beforeSuite.");
-  }
-
-  @AfterSuite
-  public void afterSuite() {
-	  System.out.println("Aftersuite.");
-  }
+	@AfterClass
+	public void afterTest() {
+		System.out.println("afterTest.");
+		//test.log(LogStatus.PASS,"Successfully executed AfterTest.");
+	}
 
 }
